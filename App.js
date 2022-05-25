75f60ad09cf29f3b779565e3b9a2fca1892b0d49
brunomaria71 <brunomaria71@yahoo.com>
2022-05-25 14:46:05 -0400

    added try catch async await to my fetch for my api

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(
+        "https://my-first-firestore-mb.web.app/restaurants"
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hi!</Text>
       <StatusBar style="auto" />
     </View>
   );
