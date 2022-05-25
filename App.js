2863e029f19b6d80ba3760ee68e43ed9088a6ce6
brunomaria71 <brunomaria71@yahoo.com>
2022-05-25 15:47:09 -0400

    added styling to app

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useEffect } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ActivityIndicator, ImageBackground } from "react-native";
+import { useEffect, useState } from "react";
 
+const image = {uri: 'https://wallpapercave.com/wp/wp5514652.png'}
 export default function App() {
-  useEffect(async () => {
+  const [allRestaurants, setAllRestaurants] = useState();
+
+  useEffect(() => {
     const getData = async () => {
       try {
         const response = await fetch(
           "https://my-first-firestore-mb.web.app/restaurants"
         );
         const data = await response.json();
-        console.log(data);
+        setAllRestaurants(data);
       } catch (err) {
         console.error(err);
       }
@@ -21,8 +24,17 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>hi!</Text>
+      <ImageBackground resizeMode='cover' source={image} style={styles.container} >
+      <Text style={styles.words}>Maria's Restaurant App</Text>
+      {allRestaurants ? (
+        allRestaurants?.map((restaurant) => {
+          return <Text key={restaurant.id} style={styles.words}>{restaurant.name}</Text>;
+        })
+      ) : (
+        <ActivityIndicator size="large" color="purple" />
+      )}
       <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -30,8 +42,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    width: '100%',
   },
+  words: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: '25',
+    fontFamily: "Cochin"
+  }
 });
