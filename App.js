142896d1a92066de962b2c9f009d1d2652f739fe
brunomaria71 <brunomaria71@yahoo.com>
2022-05-26 10:46:59 -0400

    changed photos and backgrounds, added styling

--- App.js
+++ App.js
@@ -1,57 +1,39 @@
-import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
 import {
-  Text,
-  View,
-  ActivityIndicator,
-  ImageBackground,
-  ScrollView,
-  Image,
-} from "react-native";
-import React, { useEffect, useState, Fragment } from "react";
-import styles from "./src/styles";
+  SafeAreaView, View, ActivityIndicator, ImageBackground, ScrollView
+} from 'react-native';
+import RestaurantCard from './src/components/RestaurantCard';
+import styles from './src/styles';
 
-const bgimage = {
-  uri: "https://png.pngtree.com/background/20210710/original/pngtree-restaurant-menu-background-material-picture-image_1052797.jpg",
-};
+const bgImage = { uri: 'https://png.pngtree.com/background/20210709/original/pngtree-food-western-food-steak-tomato-picture-image_941801.jpg' };
 
 export default function App() {
   const [allRestaurants, setAllRestaurants] = useState();
 
   useEffect(() => {
-    fetch("https://my-first-firestore-mb.web.app/restaurants")
-      .then((response) => response.json())
-      .then((data) => setAllRestaurants(data))
-      .catch(console.error);
-  }, []);
+    fetch('https://my-first-firestore-mb.web.app/restaurants/')
+      .then(response => response.json())
+      .then(setAllRestaurants)
+      .catch(console.error)
+  }, [])
 
   return (
     <View style={styles.container}>
-      <ImageBackground
-        resizeMode="cover"
-        source={bgimage}
-        style={styles.container}
-      >
-        <ScrollView>
-          <Text style={styles.restaurantsName}>Maria's Restaurant App</Text>
-
-          {!allRestaurants ? (
-            <ActivityIndicator size="large" color="purple" />
-          ) : (
-            allRestaurants?.map((restaurant) => {
-              return (
-                <Fragment key={restaurant.id}>
-                <Text key={restaurant.id} style={styles.restaurantsName}>
-                  {restaurant.name} / {restaurant.cuisine}
-                </Text>
-                <Image source={{ uri: restaurant.image}} style={{width: '100%', height:100}} />
-                </Fragment>
-              );
-            })
-          )}
-          <StatusBar style="auto" />
-        </ScrollView>
+      <ImageBackground resizeMode='cover' source={bgImage} style={styles.container}>
+        <SafeAreaView style={styles.container}>
+          <ScrollView>
+            {!allRestaurants
+              ? <ActivityIndicator size='large' color='orange' />
+              :
+              allRestaurants.map(singleRest => (
+                <RestaurantCard key={singleRest.id} singleRest={singleRest} />
+              ))
+            }
+          </ScrollView>
+        </SafeAreaView>
+        <StatusBar style="auto" />
       </ImageBackground>
     </View>
   );
 }
-
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,20 +1,38 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-      width: "100%",
-    },
-    restaurantsName: {
-      color: "black",
-      fontWeight: "bold",
-      fontSize: 25,
-      fontFamily: "Cochin",
-      marginVertical: 150,
-    },
-  });
-  
-  
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#00000032',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 0,
+  },
+  restaurantCard: {
+    width: '100%',
+    marginVertical: 16,
+    backgroundColor: '#fff',
+    color: '#444',
+    borderRadius: 4,
+  },
+  restaurantsName: {
+    marginHorizontal: 12,
+    fontSize: 24,
+    fontWeight: '600',
+    marginTop: 15,
+  },
+  cuisine: {
+    marginHorizontal: 12,
+    fontSize: 16,
+    fontWeight: '100',
+    color: '#ff3333',
+  },
+  address: {
+    marginHorizontal: 12,
+    fontSize: 14,
+    fontWeight: '300',
+    color: '#303030',
+    marginBottom: 8,
+  },
+});
