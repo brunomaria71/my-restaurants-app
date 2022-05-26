3c8721a906af594e8ccac7ccd68984d1980fdac3
brunomaria71 <brunomaria71@yahoo.com>
2022-05-26 09:43:07 -0400

    added a styles file

--- App.js
+++ App.js
@@ -1,55 +1,57 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ActivityIndicator, ImageBackground, ScrollView, Image, Button} from "react-native";
-import { useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  ActivityIndicator,
+  ImageBackground,
+  ScrollView,
+  Image,
+} from "react-native";
+import React, { useEffect, useState, Fragment } from "react";
+import styles from "./src/styles";
 
-const image = {uri: 'https://png.pngtree.com/background/20210710/original/pngtree-restaurant-menu-background-material-picture-image_1052797.jpg'}
+const bgimage = {
+  uri: "https://png.pngtree.com/background/20210710/original/pngtree-restaurant-menu-background-material-picture-image_1052797.jpg",
+};
 
 export default function App() {
   const [allRestaurants, setAllRestaurants] = useState();
 
   useEffect(() => {
     fetch("https://my-first-firestore-mb.web.app/restaurants")
-      .then(response => response.json())
-      .then(data => setAllRestaurants(data))
+      .then((response) => response.json())
+      .then((data) => setAllRestaurants(data))
       .catch(console.error);
   }, []);
 
   return (
     <View style={styles.container}>
-      <ImageBackground resizeMode='cover' source={image} style={styles.container} >
-      <ScrollView>
-      <Text style={styles.restaurantsName}>Maria's Restaurant App</Text>
+      <ImageBackground
+        resizeMode="cover"
+        source={bgimage}
+        style={styles.container}
+      >
+        <ScrollView>
+          <Text style={styles.restaurantsName}>Maria's Restaurant App</Text>
 
-      {allRestaurants ? (
-        allRestaurants?.map((restaurant) => {
-          return <Text key={restaurant.id} style={styles.restaurantsName}>{restaurant.name}</Text>;
-        })
-      ) : (
-        <ActivityIndicator size="large" color="purple" />
-      )}
-      <StatusBar style="auto" />
-      </ScrollView>
+          {!allRestaurants ? (
+            <ActivityIndicator size="large" color="purple" />
+          ) : (
+            allRestaurants?.map((restaurant) => {
+              return (
+                <Fragment key={restaurant.id}>
+                <Text key={restaurant.id} style={styles.restaurantsName}>
+                  {restaurant.name} / {restaurant.cuisine}
+                </Text>
+                <Image source={{ uri: restaurant.image}} style={{width: '100%', height:100}} />
+                </Fragment>
+              );
+            })
+          )}
+          <StatusBar style="auto" />
+        </ScrollView>
       </ImageBackground>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    width: '100%',
-  },
-  restaurantsName: {
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: 25,
-    fontFamily: "Cochin",
-    marginVertical: 150
-  }
-});
-
-
-      // {/* <Image source={{ uri: restaurant.image}} style={{width: '100%', height:100}} /> */}
---
+++ src/styles/index.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+      width: "100%",
+    },
+    restaurantsName: {
+      color: "black",
+      fontWeight: "bold",
+      fontSize: 25,
+      fontFamily: "Cochin",
+      marginVertical: 150,
+    },
+  });
+  
+  
